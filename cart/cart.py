a899baaa630378f4f59cd01c06169b02fe9b818e
mike <mikhailgomziak@gmail.com>
2024-01-05 18:11:40 +0200

    Cart functionality - Add.

--- cart/cart.py
+++ cart/cart.py
@@ -1,6 +1,5 @@
-import uuid
-# 9yvm91ktmttrpi8axlxayuotoha7ufe8
-# 7qfmuvqio5ie1b5w8sad219b561o3ea2
+from store.models import Product
+from decimal import Decimal
 
 
 class Cart:
@@ -9,6 +8,36 @@ class Cart:
         cart = self.session.get('session_key')
 
         if 'session_key' not in self.session:
-            cart = self.session['session_key'] = {'fav_number': 13}
+            cart = self.session['session_key'] = {}
 
         self.cart = cart
+
+    def add(self, product, product_qty):
+        product_id = str(product.id)
+
+        if product_id in self.cart:
+            self.cart[product_id]['qty'] += product_qty
+        else:
+            self.cart[product_id] = {'price': str(product.price), 'qty': product_qty}
+
+        self.session.modified = True
+
+    def __len__(self):
+        return sum(item['qty'] for item in self.cart.values())
+
+    def __iter__(self):
+        all_product_ids = list(self.cart.keys())
+        products = Product.objects.filter(id__in=all_product_ids)
+        cart = self.cart.copy()
+
+        for product in products:
+            cart[str(product.id)]['product'] = product
+
+        for item in cart.values():
+            item['price'] = Decimal(item['price'])
+            item['total'] = item['price'] * item['qty']
+            yield item
+
+    def total_price(self):
+        return sum(Decimal(item['total']) for item in self.cart.values())
+
